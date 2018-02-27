807e6bdc8f1e3e8ad1f1d12253af060d698ea183
Ralph P <perricelli.r@husky.neu.edu>
2018-02-27 14:48:30 -0500

    Final Archive

---
+++ Account.java
@@ -0,0 +1,17 @@
+
+// Represents a bank account
+public abstract class Account {
+
+    int accountNum;  // Must be unique
+    int balance;     // Must remain above zero (others Accts have more restrictions)
+    String name;     // Name on the account
+
+    public Account(int accountNum, int balance, String name){
+        this.accountNum = accountNum;
+        this.balance = balance;
+        this.name = name;
+    }
+    
+    // produce the amount available for withdrawal from this account
+    public abstract int amtAvailable();
+}
